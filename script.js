388b9f5bdc09bf1f2131258183feec2340f4c308
HoJaeChoi9636 <lostinmyemotion@gmail.com>
2026-02-04 12:01:25 +0000

    디버그

--- script.js
+++ script.js
@@ -1,8 +1,5 @@
-// 1. 책 요소를 가져옵니다.
 const book = document.getElementById('bible-book');
 
-// 2. 클릭 이벤트를 연결합니다.
 book.addEventListener('click', function() {
-    // 3. 'is-open' 클래스를 토글(껐다 켰다) 합니다.
     book.classList.toggle('is-open');
 });
